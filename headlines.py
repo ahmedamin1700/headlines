55c1cefa47a7ba14f9e3b96b89602401b5a7f62b
ahmedamin1700 <ahmedamin1700@gmail.com>
2018-09-05 19:36:41 +0200

    with jinja templates

--- headlines.py
+++ headlines.py
@@ -1,5 +1,5 @@
 import feedparser
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -10,41 +10,13 @@ RSS_FEEDS = {
     'iol': 'http://www.iol.co.za/cmlink/1.640'
 }
 
+
 @app.route('/')
-@app.route('/bbc')
-def bbc():
-    return get_news('bbc')
-
-
-@app.route('/cnn')
-def cnn():
-    return get_news('cnn')
-
-
-@app.route('/fox')
-def fox():
-    return get_news('fox')
-
-
-@app.route('/iol')
-def iol():
-    return get_news('iol')
-
-
-def get_news(publicaton):
-    feed = feedparser.parse(RSS_FEEDS[publicaton])
-    first_article = feed['entries'][0]
-    return """
-                    <html>
-                        <body>
-                            <h1>BBC Headlines</h1>
-                            <b>{0}</b> <br />
-                            <i>{1}</i> <br />
-                            <p>{2}</p> <br />
-                        </body>
-                    </html>
-               """.format(first_article.get('title'), first_article.get('published'),
-                          first_article.get('summary'))
+@app.route('/<publication>')
+def get_news(publication='bbc'):
+    feed = feedparser.parse(RSS_FEEDS[publication])
+    articles = feed['entries']
+    return render_template('home.html', articles=articles)
 
 
 if __name__ == '__main__':
