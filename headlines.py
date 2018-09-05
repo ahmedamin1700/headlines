0b26536f0f5c0342845284cbe311c430b9974d85
ahmedamin1700 <ahmedamin1700@gmail.com>
2018-09-05 20:15:09 +0200

    Post requests

--- headlines.py
+++ headlines.py
@@ -11,9 +11,9 @@ RSS_FEEDS = {
 }
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def get_news():
-    query = request.args.get('publication')
+    query = request.form.get('publication')
     if not query or not query.lower() in RSS_FEEDS:
         publication = 'bbc'
     else:
